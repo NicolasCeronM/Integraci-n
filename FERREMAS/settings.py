1287fc54ddbba0234874a7311931c21489e14603
NicolasCeronM <106988078+NicolasCeronM@users.noreply.github.com>
2024-05-08 16:05:40 -0400

    Se agrega app Producto con models

--- FERREMAS/settings.py
+++ FERREMAS/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'rest_framework',
+    'producto',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Imagenes 
+import os
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
---
+++ producto/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+
+    nombre = models.CharField(max_length=100)
+    precio = models.IntegerField()
+    descripcion = models.TextField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT)
+    imagen = models.ImageField(upload_to='producto')
+
+    def __str__(self):
+        return self.nombre
