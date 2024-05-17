4a7c2bbfbff0d78028bd91abc15dd57468b82adb
NicolasCeronM <106988078+NicolasCeronM@users.noreply.github.com>
2024-05-17 19:48:10 -0400

    Se agrega muestar de productos y logia de 'carro'

--- home/views.py
+++ home/views.py
@@ -1,11 +1,51 @@
 from django.shortcuts import render
+from producto.models import Producto, Categoria
 
 # Create your views here.
 
 def home(request):
 
-    return render(request,'index.html')
+    productos = Producto.objects.all()
+
+    data = {
+        'productos': productos
+    }
+
+    return render(request,'index.html', data)
 
 def admin(request):
 
-    return render(request,'administrador/admin.html')
+    productos = Producto.objects.all()
+
+    data = {
+        'productos': productos
+    }
+
+
+    if request.method != 'POST':
+        return render(request,'administrador/admin.html',data)
+    else:
+        imagen = request.FILES.get('imagen')
+        nombre = request.POST['nombre']
+        descipcion = request.POST['desc']
+        seccion = request.POST['seccion']
+        precio = request.POST['precio']
+
+        categoria = Categoria.objects.get(nombre=seccion)
+    
+
+        objProducto = Producto.objects.create(
+            imagen = imagen,
+            nombre = nombre,
+            descripcion = descipcion,
+            precio = precio,
+            categoria = categoria,
+            # stock = stock,
+        )
+        objProducto.save()
+
+        # messages.success(request,'Producto creado correctamente')
+
+        # return redirect(to='admin_page:productos')
+    
+        return render(request,'administrador/admin.html', data)
