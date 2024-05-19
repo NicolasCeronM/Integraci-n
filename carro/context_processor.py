2f6b4c428654e4b39530411df067445b5eb685a1
NicolasCeronM <106988078+NicolasCeronM@users.noreply.github.com>
2024-05-19 17:00:04 -0400

    Integracion primaria de compras con mercado pago

--- carro/context_processor.py
+++ carro/context_processor.py
@@ -2,7 +2,7 @@ def importe_total_carro(request):
     total=0
 
     try:
-      if request.user.is_authenticated:  
+      if request.user:  
         for key, value in request.session["carro"].items():
           total = total + (int(value["precio"]) * value["cantidad"])
           cantidad = value["cantidad"]
--- carro/views.py
+++ carro/views.py
@@ -1,11 +1,54 @@
 from django.shortcuts import render, redirect
 from .carro import Carro
 from producto.models import Producto
+import mercadopago
+from django.conf import settings
 
 # Create your views here.
 def vista_carro(request):
 
-    return render(request,'carro.html')
+    sdk = mercadopago.SDK(settings.MERCADO_PAGO_ACCESS_TOKEN)
+
+     # Instancia del carrito de compras
+    carro = Carro(request)
+
+    # Verificar si el carrito está vacío
+    if not carro.carro:
+        # Redirigir a una página de productos o mostrar un mensaje de carrito vacío
+        return render(request, 'carro.html')
+
+    # Construir el atributo "items" basado en los productos del carrito
+    items = []
+    for key, producto in carro.carro.items():
+        item = {
+            "title": producto['nombre'],  
+            "quantity": producto['cantidad'],  
+            "currency_id": "CLP",  
+            "unit_price": float(producto['precio'])  
+        }
+        items.append(item)
+    
+    preference_data = {
+        "items": items,
+        "payer": {  
+            "email": "test_user@test.com"
+        },
+        "back_urls": {
+            "success": "http://localhost:8000/success/",
+            "failure": "http://localhost:8000/failure/",
+            "pending": "http://localhost:8000/pending/"
+        },
+        "auto_return": "approved"
+    }
+    
+    preference_response = sdk.preference().create(preference_data)
+    preference = preference_response["response"]
+    
+    return render(request, 'carro.html', {
+        'preference_id': preference['id'],
+        'public_key': settings.MERCADO_PAGO_PUBLIC_KEY
+    })
+
     
 def agregar_producto(request,producto_id):
 
@@ -25,7 +68,7 @@ def eliminiar_producto(request,producto_id):
 
     carro.eliminar(producto = producto)
 
-    return redirect("index")
+    return redirect("carro:carro")
 
 def restar_producto(request,producto_id):
 
@@ -35,11 +78,11 @@ def restar_producto(request,producto_id):
 
     carro.restar_producto(producto = producto)
 
-    return redirect("dashboard:carro")
+    return redirect("carro:carro")
 
 def limpiar_carro(request):
 
     carro = Carro(request)
     carro.limpiar_carro()
 
-    return redirect("dashboard:carro")
+    return redirect("carro:carro")
