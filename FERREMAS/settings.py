06a3a92a99f397c2c1ac8a647876f32cecd5be64
NicolasCeronM <106988078+NicolasCeronM@users.noreply.github.com>
2024-05-22 19:19:06 -0400

    Modificaciones en la base de datos

--- FERREMAS/settings.py
+++ FERREMAS/settings.py
@@ -80,14 +80,23 @@ WSGI_APPLICATION = 'FERREMAS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'ferremas',
+#         'USER':'root',
+#         'HOST': 'localhost',
+#         'PASSWORD':'',
+#         'PORT':'3306',
+#     }
+# }
+
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ferremas',
-        'USER':'root',
-        'HOST': 'localhost',
-        'PASSWORD':'',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
