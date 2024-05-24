5b767f7095aadb26b33858e79ac70cb83735537c
Jean Carlos <omashu10k@gmail.com>
2024-05-24 14:38:38 -0400

    Changes to be committed: 	modified:   FERREMAS/__pycache__/settings.cpython-310.pyc 	modified:   FERREMAS/settings.py 	modified:   carro/templates/carro.html 	modified:   db.sqlite3 	modified:   home/__pycache__/urls.cpython-310.pyc 	modified:   home/__pycache__/views.cpython-310.pyc

--- FERREMAS/settings.py
+++ FERREMAS/settings.py
@@ -77,28 +77,27 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'FERREMAS.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': 'ferremas',
-#        'USER':'root',
-#        'HOST': 'localhost',
-#        'PASSWORD':'',
-#        'PORT':'3306',
-#    }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ferremas',
+        'USER': 'root',
+        'HOST': 'localhost',
+        'PASSWORD': '',
+        'PORT': '3308',
     }
 }
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#
+#    }
+# }
 
 
 # Password validation
